87da53b8ff713d0393f8656751be59d315f992d4
Hexmur <mauroalizzi@gmail.com>
2025-12-06 19:19:15 +0100

    Implemented button function

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 const container = document.querySelector("#container");
 
 function initializeRows(){
-  for(let i=0; i<4; i++){
-    rows[i] = document.getElementById(`row${i}`);
+  for(let i=0; i<gridDimension; i++){
+    //rows[i] = document.getElementById(`row${i}`);
+    rows[i] = document.createElement("div");
+    rows[i].id = `row${i}`;
+    rows[i].className = "row";
+    container.appendChild(rows[i]);
   }
 }
 
 function initializeDivs(){
-  for(let i=0; i<4; i++){
-    for(let j=0; j<4; j++){
+  count = 0;
+  for(let i=0; i<gridDimension; i++){
+    for(let j=0; j<gridDimension; j++){
       divs[count] = document.createElement("div");
       //divs[count].textContent = `${count}`;
       rows[i].appendChild(divs[count]);
 
       //divs[count].style.flex = "1";
       divs[count].id = "item";
-      divs[count].style.width = "60px";
-      divs[count].style.height = "60px";
+      divs[count].style.width = "20px";
+      divs[count].style.height = "20px";
       divs[count].style.alignContent = "center";
-      divs[count].style.border = "solid red"
       count++;
     }
   }
 }
+
+function deleteGrid(oldDimension){
+  for(let i=0; i<oldDimension; i++){
+    rows[i].remove();
+    for(let j=0; j<oldDimension; j++){
+      divs[j].remove();
+    }
+  }
+}
+
+let gridDimension = 4;
 
 const rows = [];
 initializeRows();
@@ -41,4 +56,15 @@ addEventListener("mouseout", (event)=>{
   if(event.target.id === "item"){
     event.target.style.backgroundColor = "white";
   }
+});
+
+const button = document.getElementById("btnChange");
+let value = 0;
+
+button.addEventListener("click", (event)=>{
+  deleteGrid(gridDimension);
+  gridDimension = prompt("Choose the grid dimension:");
+  
+  initializeRows();
+  initializeDivs();
 });
